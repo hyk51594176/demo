444ffa79a0dbccfd2cf7ae22827e9194e019b671
hyk51594176 <51594176@qq.com>
2019-12-31 10:34:08 +0000

    Deploying to gh-pages from  d466733a23ebf3a8a9b27ea8caba8e3a1bb73a02

---
+++ precache-manifest.a2aebf568a069b3c8702e0cf12041c0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e3c837f1312b12a5d2f10004e12180c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f109e060593f47de677b",
+    "url": "./static/css/main.1079f29a.chunk.css"
+  },
+  {
+    "revision": "a2a9314b0898f7684ab2",
+    "url": "./static/js/2.ad80f138.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "./static/js/2.ad80f138.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f109e060593f47de677b",
+    "url": "./static/js/main.88471013.chunk.js"
+  },
+  {
+    "revision": "fb1f82c2d0b0ea233982",
+    "url": "./static/js/runtime-main.2b5aea54.js"
+  },
+  {
+    "revision": "bdf05f515678c9befe77e0c442523a24",
+    "url": "./static/media/Rufina-Bold.bdf05f51.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.713d975b05aa724a842c13fdac181563.js"
+  "./precache-manifest.a2aebf568a069b3c8702e0cf12041c0c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

b1a004d94d4b94d15110e8f1eec8b18d67ef7e63
yukai.han <yukai.han@st-creditech.com>
2019-12-31 16:05:55 +0800

    deploy

---
+++ precache-manifest.713d975b05aa724a842c13fdac181563.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c209a74e9e01cd94c6712176414f4b03",
+    "url": "./index.html"
+  },
+  {
+    "revision": "70e2c157ebc82553c577",
+    "url": "./static/css/main.1079f29a.chunk.css"
+  },
+  {
+    "revision": "a2a9314b0898f7684ab2",
+    "url": "./static/js/2.ad80f138.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "./static/js/2.ad80f138.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70e2c157ebc82553c577",
+    "url": "./static/js/main.f9cf4d43.chunk.js"
+  },
+  {
+    "revision": "fb1f82c2d0b0ea233982",
+    "url": "./static/js/runtime-main.2b5aea54.js"
+  },
+  {
+    "revision": "bdf05f515678c9befe77e0c442523a24",
+    "url": "./static/media/Rufina-Bold.bdf05f51.ttf"
+  }
+]);

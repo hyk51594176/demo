73c8c076c73d31b20a0c8d4a963190f3585346fb
yukai.han <yukai.han@st-creditech.com>
2020-01-15 16:01:38 +0800

    test

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './pages/App'
 import * as serviceWorker from './serviceWorker'
-import { Provider } from '@hanyk/usestore'
+import { Provider } from './utils/store'
 import { initState, reducer } from './utils/reducer'
 
 ReactDOM.render(
